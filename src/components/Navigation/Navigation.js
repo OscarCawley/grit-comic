ac19bcd6c90f39fe2aaef6512967a521589d21f7
Oscar Cawley <oscar.cawley@outlook.com>
2025-04-24 16:24:46 +0100

    Add Navigation component styling for improved layout and user experience

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <div>
+        <div className='navigation'>
             <Link to="/chapters"><button>CHAPTERS</button></Link>
             <Link to="/wiki"><button>WIKI</button></Link>
             <Link to="/updates"><button>UPDATES</button></Link>

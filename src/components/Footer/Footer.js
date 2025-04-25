0025e95e1935a5124471cee015f06eee34690847
Oscar Cawley <oscar.cawley@outlook.com>
2025-04-25 11:02:41 +0100

    Moved socials back to navigation bar and removed from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,17 +7,9 @@ import bluesky from '../../assets/icons/bluesky.png';
 
 const Footer = () => {
     return (
-        <div>
-            <footer className="footer">
-                <div className='socials'>
-                    <a href="https://www.youtube.com/@Gh0st_Zer-0" target="_blank" rel="noopener noreferrer"><img src={youtube} alt="Youtube" /></a>
-                    <a href="https://www.instagram.com/gh0st_zer0_/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram" /></a>
-                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter" /></a>
-                    <a href="https://bsky.app/profile/jdlowden.bsky.social" target="_blank" rel="noopener noreferrer"><img src={bluesky} alt="Bluesky" /></a>
-                </div>
-                <p>Designed & Developed by Oscar Cawley</p>
-            </footer>
-        </div>
+        <footer className="footer">
+            <p>Designed & Developed by Oscar Cawley</p>
+        </footer>
     );
 };
 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import './Navigation.css';
+import youtube from '../../assets/icons/youtube.png';
+import instagram from '../../assets/icons/instagram.png';
+import twitter from '../../assets/icons/twitter.png';
+import bluesky from '../../assets/icons/bluesky.png';
 
 const Navigation = () => {
     return (
         <div className='navigation'>
-            <Link to="/chapters"><button>CHAPTERS</button></Link>
-            <Link to="/wiki"><button>WIKI</button></Link>
-            <Link to="/updates"><button>UPDATES</button></Link>
-            <Link to="/support"><button>SUPPORT</button></Link>
+            <div className='main-buttons'>
+                <Link to="/chapters"><button>CHAPTERS</button></Link>
+                <Link to="/wiki"><button>WIKI</button></Link>
+                <Link to="/updates"><button>UPDATES</button></Link>
+                <Link to="/support"><button>SUPPORT</button></Link>
+            </div>
+            <div className='socials'>
+                <a href="https://www.youtube.com/@Gh0st_Zer-0" target="_blank" rel="noopener noreferrer"><img src={youtube} alt="Youtube" /></a>
+                <a href="https://www.instagram.com/gh0st_zer0_/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram" /></a>
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter" /></a>
+                <a href="https://bsky.app/profile/jdlowden.bsky.social" target="_blank" rel="noopener noreferrer"><img src={bluesky} alt="Bluesky" /></a>
+            </div>
         </div>
     );
 };

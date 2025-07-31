b1db8cae3d24443169060fbc398450aa0fc3ef04
Oscar Cawley <oscar.cawley@outlook.com>
2025-07-31 13:32:04 +0100

    Created basic foundation for getting updates from backend and listing

--- client/src/pages/Updates/Updates.js
+++ client/src/pages/Updates/Updates.js
@@ -1,8 +1,42 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Updates.css';
+import PageAnimation from '../../components/PageAnimation/PageAnimation.js';
 
 const Updates = () => {
+    const [updates, setUpdates] = useState([]);
+
+    useEffect (() => {
+        fetchUpdates();
+    }, []);
+
+    const fetchUpdates = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/api/updates');
+            console.log('API raw response:', response);
+            console.log('API response data:', response.data);
+            setUpdates(response.data);
+        } catch (error) {
+            console.error('Error fetching updates:', error);
+        }
+    }
+
     return (
-        <div></div>
+        <PageAnimation>
+            <div className='updates'>
+                <h1>Updates</h1>
+                <div className="updates-list">
+                    {updates.map((update) => (
+                        <div key={update.id} className="update-item">
+                            <h2>{update.title}</h2>
+                            <p>{update.content}</p>
+                            <p>{update.updated_at}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </PageAnimation>
     );
 };
 

4aea7d80d9114ae29eeee34b3bab5fa1fdef1e36
Oscar Cawley <oscar.cawley@outlook.com>
2025-07-31 13:06:15 +0100

    Added page animation

--- client/src/pages/Wiki/Wiki.js
+++ client/src/pages/Wiki/Wiki.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './Wiki.css';
 import DOMPurify from 'dompurify';
+import PageAnimation from '../../components/PageAnimation/PageAnimation.js';
 
 const Wiki = () => {
     const [allPosts, setAllPosts] = useState([]);
@@ -37,30 +38,32 @@ const Wiki = () => {
     }
 
     return (
-        <div className='wiki'>
-            <div className="category-list">
-                <h1>Category</h1>
-                <button className={`category-button ${activeCategory === "All" ? "active" : ""}`} onClick={() => handleCategoryClick("All")}>All</button>
-                <button className={`category-button ${activeCategory === "General" ? "active" : ""}`} onClick={() => handleCategoryClick("General")}>General</button>
-                <button className={`category-button ${activeCategory === "World" ? "active" : ""}`} onClick={() => handleCategoryClick("World")}>World</button>
-                <button className={`category-button ${activeCategory === "Characters" ? "active" : ""}`} onClick={() => handleCategoryClick("Characters")}>Characters</button>
-            </div>
-            <div className="wiki-list">
-                <h1>Wiki</h1>
-                {posts.map((post) => (
-                    <div key={post.id} className="wiki-item">
-                        <div className='wiki-item-header'>
-                            <Link to={`/wiki/${post.slug}`} className='wiki-item-link'>
-                                <h2>{post.title}</h2>
-                            </Link>
-                            <span>{post.category_name}</span>
+        <PageAnimation>
+            <div className='wiki'>
+                <div className="category-list">
+                    <h1>Category</h1>
+                    <button className={`category-button ${activeCategory === "All" ? "active" : ""}`} onClick={() => handleCategoryClick("All")}>All</button>
+                    <button className={`category-button ${activeCategory === "General" ? "active" : ""}`} onClick={() => handleCategoryClick("General")}>General</button>
+                    <button className={`category-button ${activeCategory === "World" ? "active" : ""}`} onClick={() => handleCategoryClick("World")}>World</button>
+                    <button className={`category-button ${activeCategory === "Characters" ? "active" : ""}`} onClick={() => handleCategoryClick("Characters")}>Characters</button>
+                </div>
+                <div className="wiki-list">
+                    <h1>Wiki</h1>
+                    {posts.map((post) => (
+                        <div key={post.id} className="wiki-item">
+                            <div className='wiki-item-header'>
+                                <Link to={`/wiki/${post.slug}`} className='wiki-item-link'>
+                                    <h2>{post.title}</h2>
+                                </Link>
+                                <span>{post.category_name}</span>
+                            </div>
+                            <div className='wiki-item-content' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content) }}></div>
+                            <p className='wiki-item-updated'>{post.updated_at_formatted}</p>
                         </div>
-                        <div className='wiki-item-content' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.content) }}></div>
-                        <p className='wiki-item-updated'>{post.updated_at_formatted}</p>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
+        </PageAnimation>
     );
 };
 

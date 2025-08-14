05bf1c12362e892aee71d327d0cf07bc4b60811f
Oscar Cawley <oscar.cawley@outlook.com>
2025-08-14 14:45:06 +0100

    Changed GET request to send formatted time stamps

--- server/routes/updates.js
+++ server/routes/updates.js
@@ -5,7 +5,10 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const [updates] = await db.query('SELECT * FROM updates ORDER BY created_at DESC');
+        const [updates] = await db.query(`
+            SELECT *, DATE_FORMAT(updates.created_at, '%d/%m/%Y %H:%i') AS created_at_formatted, 
+            DATE_FORMAT(updates.updated_at, '%d/%m/%Y %H:%i') AS updated_at_formatted
+            FROM updates ORDER BY created_at DESC`);
         res.json(updates);
     } catch (err) {
         console.error(err);

0b8ef2dfe53ea37d98f56c1e979504518f044382
Oscar Cawley <oscar.cawley@outlook.com>
2025-08-20 14:52:31 +0100

    Added functionality to fetch the FAQ questions and display them

--- client/src/pages/Support/Support.js
+++ client/src/pages/Support/Support.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import DOMPurify from 'dompurify';
+import './Support.css';
 
 const Support = () => {
+    const [faq, setFaq] = useState([]);
+
+    useEffect (() => {
+        fetchFaq();
+    }, []);
+
+    const fetchFaq = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/api/faq');
+            setFaq(response.data);
+        } catch (error) {
+            console.error('Error fetching FAQ:', error);
+        }
+    }
+
     return (
-        <div></div>
+        <div>
+            <h1>Support</h1>
+            <div className="faq-list">
+                {faq.map((item) => (
+                    <div key={item.id} className="faq-item">
+                        <h2 className='faq-item-question'>{item.question}</h2>
+                        <div className='faq-item-answer' dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(item.answer) }}></div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
 

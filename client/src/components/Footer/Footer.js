776c022fccdf3b6a801ed5e27d47b20d72f6b288
Oscar Cawley <oscar.cawley@outlook.com>
2025-08-21 15:04:08 +0100

    Removed unused variables

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,9 +1,4 @@
-import React from 'react';
 import './Footer.css';
-import youtube from '../../assets/icons/youtube.png';
-import instagram from '../../assets/icons/instagram.png';
-import twitter from '../../assets/icons/twitter.png';
-import bluesky from '../../assets/icons/bluesky.png';
 
 const Footer = () => {
     return (

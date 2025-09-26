778c0b03b35a4a135a9ccfa9e49fcc648a3cf571
Oscar Cawley <oscar.cawley@outlook.com>
2025-09-26 12:26:42 +0100

    Changed gitignore to include sql scripts

---
+++ server/sql-scripts/initial-faq.sql
@@ -0,0 +1,4 @@
+INSERT INTO faq (question, answer) VALUES
+('What is Grit Comic?', 'Grit Comic is a webcomic platform that allows users to read and create comics online.'),
+('How do I create an account?', 'You can create an account by clicking on the "Sign Up" button on the homepage and filling out the registration form.'),
+('Is Grit Comic free to use?', 'Yes, Grit Comic is free to use. However, some features may require a premium subscription in the future.');

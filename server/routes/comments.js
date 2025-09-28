5283c21c679cdfc9e36c65a57b60a84dc24e9413
Oscar Cawley <oscar.cawley@outlook.com>
2025-09-28 13:09:48 +0100

    Edited SQL query for get request to join the comments and user tables, get the username for the comment and format the date

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const [comments] = await db.query('SELECT * FROM comments ORDER BY created_at ASC');
+        const [comments] = await db.query(`SELECT comments.*, users.username AS username, DATE_FORMAT(comments.created_at, '%d/%m/%Y %H:%i') AS created_at_formatted FROM comments JOIN users ON comments.user_id = users.id ORDER BY created_at ASC`);
         res.json(comments);
     } catch (err) {
         console.error(err);

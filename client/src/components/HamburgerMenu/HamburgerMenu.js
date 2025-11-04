f07e4b8c84f264786da7b941d41673f2f400b63c
Oscar Cawley <oscar.cawley@outlook.com>
2025-11-04 13:30:27 +0000

    Fixed hamburger and navigation bar conditional display and CSS design for hamburger menu

--- client/src/components/HamburgerMenu/HamburgerMenu.js
+++ client/src/components/HamburgerMenu/HamburgerMenu.js
@@ -2,6 +2,10 @@ import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { UserContext } from "../../context/UserContext";
 import './HamburgerMenu.css';
+import youtube from '../../assets/icons/youtube.png';
+import instagram from '../../assets/icons/instagram.png';
+import twitter from '../../assets/icons/twitter.png';
+import bluesky from '../../assets/icons/bluesky.png';
 
 const HamburgerMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -32,17 +36,23 @@ const HamburgerMenu = () => {
         <ul>
           {user && (
             <li>
-              <Link to="/account" onClick={() => setMenuOpen(false)}>
+              <Link className="hamburger-link" to="/account" onClick={() => setMenuOpen(false)}>
                 {user.username}
               </Link>
             </li>
           )}
-          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/chapters" onClick={() => setMenuOpen(false)}>Chapters</Link></li>
-          <li><Link to="/wiki" onClick={() => setMenuOpen(false)}>Wiki</Link></li>
-          <li><Link to="/updates" onClick={() => setMenuOpen(false)}>Updates</Link></li>
-          <li><Link to="/support" onClick={() => setMenuOpen(false)}>Support</Link></li>
+          <li><Link className="hamburger-link" to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+          <li><Link className="hamburger-link" to="/chapters" onClick={() => setMenuOpen(false)}>Chapters</Link></li>
+          <li><Link className="hamburger-link" to="/wiki" onClick={() => setMenuOpen(false)}>Wiki</Link></li>
+          <li><Link className="hamburger-link" to="/updates" onClick={() => setMenuOpen(false)}>Updates</Link></li>
+          <li><Link className="hamburger-link" to="/support" onClick={() => setMenuOpen(false)}>Support</Link></li>
         </ul>
+        <div className="hamburger-socials">
+          <a href="https://www.youtube.com/@Gh0st_Zer-0" target="_blank" rel="noopener noreferrer"><img src={youtube} alt="Youtube" /></a>
+          <a href="https://www.instagram.com/gh0st_zer0_/" target="_blank" rel="noopener noreferrer"><img src={instagram} alt="Instagram" /></a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><img src={twitter} alt="Twitter" /></a>
+          <a href="https://bsky.app/profile/jdlowden.bsky.social" target="_blank" rel="noopener noreferrer"><img src={bluesky} alt="Bluesky" /></a>
+        </div>
       </nav>
     </>
   );

2c56cf9ce71654fae42a85f31719dc6ed4396fa6
Oscar Cawley <oscar.cawley@outlook.com>
2025-11-19 11:48:34 +0000

    Improved readability

--- client/src/pages/UnsubscribePage/UnsubscribePage.js
+++ client/src/pages/UnsubscribePage/UnsubscribePage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './UnsubscribePage.css';
 import useMinLoading from '../../hooks/useMinLoading';

72a5a8e500d2a3d3c2eea4b2e7bd48e15c289610
Oscar Cawley <oscar.cawley@outlook.com>
2025-11-19 12:05:10 +0000

    Removed reading advice from footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,6 @@ const Footer = () => {
     return (
         <footer className="footer">
             <p>Designed & Developed by Oscar Cawley</p>
-            <p className='help'>For a better reading experience, please use a desktop</p>
         </footer>
     );
 };
